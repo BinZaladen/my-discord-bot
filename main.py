873d7ccd12dd5e53dc1ec04784381f04c4c9768a
BinZaladen <kamienzachodni@gmail.com>
2025-06-05 18:54:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.message_content = True  # Wymagane do odczytu treści wiadomości
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -19,5 +21,4 @@ token = os.getenv("DISCORD_TOKEN")
 if token is None:
     print("Błąd: Brak tokena Discorda.")
 else:
-    client = discord.Client()
-    client.run(token)
+    bot.run(token)
