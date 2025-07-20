b3c8b2572810433306b33de0d52290e4c4dbd6c7
BinZaladen <kamienzachodni@gmail.com>
2025-07-20 22:36:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,85 +6,96 @@ from discord.ui import View, Button
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Zmienna środowiskowa z tokenem
-TOKEN = os.getenv("DISCORD_TOKEN")
+class TicketButton(Button):
+    def __init__(self):
+        super().__init__(
+            label="Kup wybrane itemy",
+            style=discord.ButtonStyle.green,
+            url="https://discord.com/channels/1373253103174604810/1373305137228939416"  # Link do kanału ticketów
+        )
 
-# ID kanału ticketów
-TICKET_CHANNEL_ID = 1373305137228939416
-
-# URL do loga serwera
-SERVER_LOGO_URL = "https://i.imgur.com/NJZ5Fzj.png"  # Podmień na swoje logo!
-
-OFFERS = {
-    1373266589310517338: (
-        "<:elytra:1374797373406187580> Elytra — 12zł\n"
-        "<:buty:1374796797222064230> Buty flasha — 5zł\n"
-        "<:miecz:1374791139462352906> Miecz 6 — 3zł\n"
-        "<:shulker:1374795916531335271> Shulker s2 — 7zł\n"
-        "<:shulker:1374795916531335271> Shulker totemów — 6zł\n\nObraz"
-    ),
-    1373267159576481842: (
-        "<:klata:1374793644246306866> Set 25 — 30zł\n"
-        "<:miecz:1374791139462352906> Miecz 25 — 25zł\n"
-        "<:kilof:1374795407493959751> Kilof 25 — 10zł\n"
-        "💸 1mln$ — 18zł\n\nObraz"
-    ),
-    1373268875407396914: (
-        "💵 4,5k$ — 1zł\n💸 50k$ — 12zł\n💸 550k$ — 130zł\n"
-        "<:ANA2:137479901735931494> Anarchiczny set 2 — 28zł\n"
-        "<:klata:1374793644246306866> Anarchiczny set 1 — 9zł\n\n🎉 Eventówki:\n"
-        "<:miecz:1374791139462352906> Anarchiczny miecz — 3zł\n"
-        "<:MieczZajeczy:1375486003891929088> Zajęczy miecz — 65zł\n"
-        "<:totem:1374788635211206757> Totem ułaskawienia — 630zł\n"
-        "<:exalibur:1374785662191927416> Excalibur — 370zł"
-    ),
-    1373270295556788285: (
-        "💵 50k$ — 1zł\n💸 1mln$ — 33zł\n\n🎉 Eventówki:\n"
-        "<:exalibur:1374785662191927416> Excalibur — 111zł\n"
-        "<:totem:1374788635211206757> Totem ułaskawienia — 270zł\n"
-        "<:sakiewka:1374799829120716892> Sakiewka — 50zł\n\nObraz"
-    ),
-    1373273108093337640: (
-        "💸 10mld$ — 2zł\n<:miecz:1374791139462352906> Miecz 35 — 65zł\n"
-        "<:klata:1374793644246306866> Set 35 — 90zł\n\nObraz"
-    ),
-    1374380939970347019: (
-        "💵 15k$ — 1zł\n<:buda:1375488639496093828> Buda — 30zł\n"
-        "<:LoveSwap:1375490111801790464> Love swap — 100zł\n"
-        "<:KlataMeduzy:1375487632531918875> Klata meduzy — 140zł\n\nObraz"
-    ),
-}
+class OfertaView(View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(TicketButton())
 
 @bot.event
 async def on_ready():
-    print(f"Zalogowano jako {bot.user} (ID: {bot.user.id})")
+    print(f"Zalogowano jako {bot.user.name}")
+    
+    # Tu wklejamy oferty
+    oferty = [
+        {
+            "channel_id": 1373266589310517338,
+            "content": """🛒 **Oferta itemów na sprzedaż**
+<:Elytra:1374797373406187580> Elytra — 12zł
+<:Buty:1374796797222064230> Buty flasha — 5zł
+<:Miecz:1374791139462352906> Miecz 6 — 3zł
+<:Shulker:1374795916531335271> Shulker s2 — 7zł
+<:Shulker:1374795916531335271> Shulker totemów — 6zł"""
+        },
+        {
+            "channel_id": 1373267159576481842,
+            "content": """🛒 **Oferta itemów na sprzedaż**
+<:Klata:1374793644246306866> Set 25 — 30zł
+<:Miecz:1374791139462352906> Miecz 25 — 25zł
+<:Kilof:1374795407493959751> Kilof 25 — 10zł
+💸 1mln$ — 18zł"""
+        },
+        {
+            "channel_id": 1373268875407396914,
+            "content": """🛒 **Oferta itemów na sprzedaż**
+💵 4,5k$ — 1zł
+💸 50k$ — 12zł
+💸 550k$ — 130zł
+<:ANA2:137479901735931494> Anarchiczny set 2 — 28zł
+<:Klata:1374793644246306866> Anarchiczny set 1 — 9zł
 
-    for channel_id, offer_text in OFFERS.items():
-        channel = bot.get_channel(channel_id)
-        if not channel:
-            print(f"Nie znaleziono kanału o ID {channel_id}")
-            continue
+🎉 **Eventówki**
+<:Miecz:1374791139462352906> Anarchiczny miecz — 3zł
+<:MieczZajeczy:1375486003891929088> Zajęczy miecz — 65zł
+<:Totem:1374788635211206757> Totem ułaskawienia — 630zł
+<:Excalibur:1374785662191927416> Excalibur — 370zł"""
+        },
+        {
+            "channel_id": 1373270295556788285,
+            "content": """🛒 **Oferta itemów na sprzedaż**
+💵 50k$ — 1zł
+💸 1mln$ — 33zł
 
-        # Usuń stare wiadomości bota
-        async for message in channel.history(limit=50):
-            if message.author == bot.user:
-                await message.delete()
+🎉 **Eventówki**
+<:Excalibur:1374785662191927416> Excalibur — 111zł
+<:Totem:1374788635211206757> Totem ułaskawienia — 270zł
+<:Sakiewka:1374799829120716892> Sakiewka — 50zł"""
+        },
+        {
+            "channel_id": 1373273108093337640,
+            "content": """🛒 **Oferta itemów na sprzedaż**
+💸 10mld$ — 2zł
+<:Miecz:1374791139462352906> Miecz 35 — 65zł
+<:Klata:1374793644246306866> Set 35 — 90zł"""
+        },
+        {
+            "channel_id": 1374380939970347019,
+            "content": """🛒 **Oferta itemów na sprzedaż**
+💵 15k$ — 1zł
+<:Buda:1375488639496093828> Buda — 30zł
+<:LoveSwap:1375490111801790464> Love swap — 100zł
+<:KlataMeduzy:1375487632531918875> Klata meduzy — 140zł"""
+        }
+    ]
 
-        embed = discord.Embed(
-            title="🛒 Oferta itemów na sprzedaż",
-            description=offer_text,
-            color=discord.Color.blue()
-        )
-        embed.set_thumbnail(url=SERVER_LOGO_URL)
+    logo_url = "https://cdn.discordapp.com/icons/1373253103174604810/4e244f4eaa6c447fd59cfeec3a6a8df6.webp"  # Link do logo
 
-        ticket_channel = bot.get_channel(TICKET_CHANNEL_ID)
-        button_url = f"https://discord.com/channels/{ticket_channel.guild.id}/{ticket_channel.id}" if ticket_channel else "https://discord.com"
-
-        view = View()
-        view.add_item(Button(label="Utwórz ticket", style=discord.ButtonStyle.link, url=button_url))
-
-        await channel.send(embed=embed, view=view)
-
-    print("Wszystkie oferty zostały wysłane.")
+    for oferta in oferty:
+        channel = bot.get_channel(oferta["channel_id"])
+        if channel:
+            embed = discord.Embed(
+                description=oferta["content"],
+                color=discord.Color.blue()
+            )
+            embed.set_thumbnail(url=logo_url)
+            await channel.purge(limit=5)
+            await channel.send(embed=embed, view=OfertaView())
 
 bot.run(os.getenv("DISCORD_TOKEN"))
